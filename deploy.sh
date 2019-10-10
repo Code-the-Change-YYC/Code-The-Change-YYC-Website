83008cb8e9ebd889c3c0c337d637a4468bd2b2f4
Camilo Rostoker <crostoker@benevity.com>
2019-10-09 22:12:23 -0600

    tweak wording on cause CTA and also re-add post-secondary partner titles but with different copy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ if [ "$#" -ne 1 ]; then
    echo "Usage: deploy.sh <filename>"
    exit;
 fi
-echo $#
 
 # Assumes you have an entry called "codethechange" in your ~/.aws/credentials file 
 aws s3 cp $1 s3://codethechangeyyc.ca --profile codethechange
