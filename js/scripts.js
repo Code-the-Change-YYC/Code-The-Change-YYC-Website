bad53a4bb00447845d28d73111faa1369105d7e9
Cole Thiessen <ckthiessen@hotmail.com>
2019-10-21 21:16:35 -0600

    Added cause registration form. Sends POST request to google-form API to update google forms.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,17 @@
    Description: Custom JS file
 */
 
+// Partner With Us Form
+$(document).on('submit', '#partner-form', function (event) {
+  let thankYou = $(".thank-you");
+  thankYou.show();
+  thankYou.fadeOut(15000);
+  $("#partner-form")[0].reset();
+ return window.submitGoogleForm(this, function() {
+    $("#partner-form")[0].reset();
+  });
+});
+
 
 (function($) {
     "use strict"; 
