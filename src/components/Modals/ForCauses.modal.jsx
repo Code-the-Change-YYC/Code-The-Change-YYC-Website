d32cc9a1a53868697f88ea8f98978ec9c9ca88aa
Christian Garrovillo <me@christiang.dev>
2021-01-10 13:42:59 -0700

    Fixes Links

--- src/components/Modals/ForCauses.modal.jsx
+++ src/components/Modals/ForCauses.modal.jsx
@@ -9,7 +9,9 @@ export default function CausesModal({ show, onHide, ...rest }) {
       onHide={onHide}
       img={<img className='img-fluid' src='images/_causesVector.jpg' alt='alternative' />}
       cta={
-        <a className='btn-solid-reg mfp-close page-scroll' href=''>
+        <a
+          className='btn-solid-reg mfp-close page-scroll'
+          href='https://docs.google.com/forms/d/e/1FAIpQLSfOSVxP6L0dCUVeyWX1yI-jp1pYesaSpqz2-7i8z_JaiRu4YA/viewform?usp=sf_link'>
           REGISTER
         </a>
       }
